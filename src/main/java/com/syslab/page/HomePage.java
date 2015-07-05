e6015cf2a8cc2ad61fe2f6d31ca2a5ff9ad7b122
gmazzei <gmazzei91@gmail.com>
2015-07-05 01:01:47 -0300

    File uploader

--- src/main/java/com/syslab/page/HomePage.java
+++ src/main/java/com/syslab/page/HomePage.java
@@ -1,7 +1,13 @@
 package com.syslab.page;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.upload.FileUpload;
+import org.apache.wicket.markup.html.form.upload.FileUploadField;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import com.syslab.service.UserService;
@@ -12,7 +18,28 @@ public class HomePage extends WebPage {
 	private UserService userService;
 	
 	public HomePage() {
-		add(new Label("message", userService.getAll().size()));
+		
+		Form form = new Form("form");
+		add(form);
+		
+		final FileUploadField fileUploader = new FileUploadField("fileUploader");
+		fileUploader.setOutputMarkupId(true);
+		form.add(fileUploader);
+		
+		AjaxButton ajaxButton = new AjaxButton("submit", Model.of("Analizar")) {
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				FileUpload fileUpload = fileUploader.getFileUpload();
+				byte[] fileBytes = fileUpload.getBytes();
+				
+				//TODO: Analize image
+				
+				super.onSubmit(target, form);
+			}
+			
+		};
+		form.add(ajaxButton);
     }
 	
 }
